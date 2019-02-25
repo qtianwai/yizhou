9e60a066ebf59fcb7eab8535e36c4591fe46542a
unknown <xzy@xzyOffice-PC.smada.gov.cn>
2019-02-25 18:46:17 +0800

    aa

--- 完整源码/config/production_setting.py
+++ 完整源码/config/production_setting.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 DEBUG = True
 SQLALCHEMY_ECHO = True
-SQLALCHEMY_DATABASE_URI = 'mysql://root:58plPi56SdYP8a63@127.0.0.1/food_db?charset=utf8mb4'
+SQLALCHEMY_DATABASE_URI = 'mysql://root:admin@140.82.37.13/pythonflask?charset=utf8mb4'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ENCODING = "utf8mb4"
 
@@ -9,4 +9,4 @@ APP = {
     'domain':'https://food.54php.cn'
 }
 
-RELEASE_VERSION="20180729001"
+RELEASE_VERSION="20190225001"
