c29ebfbcdaf3f54158015dd4218cac55ece16356
unknown <xzy@xzyOffice-PC.smada.gov.cn>
2019-02-28 10:29:22 +0800

    a

--- 完整源码/config/production_setting.py
+++ 完整源码/config/production_setting.py
@@ -5,8 +5,8 @@ SQLALCHEMY_DATABASE_URI = 'mysql://root:admin@140.82.37.13/pythonflask?charset=u
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ENCODING = "utf8mb4"
 
-# APP = {
-#     'domain':'https://food.54php.cn'
-# }
+APP = {
+    'domain':'https://food.54php.cn'
+}
 
 RELEASE_VERSION="20190225001"
